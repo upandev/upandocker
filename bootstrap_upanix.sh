075c19a1ad742f320dbcb9e04320a82e1ebda357
Prajwala Prabhakar <srinivasa.prajwal@gmail.com>
2022-01-30 19:16:53 +0800

    upanix docker container build config

---
+++ bootstrap_upanix.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#	 Upanix - An x86 based Operating System
+#	 Copyright (C) 2011 'Prajwala Prabhakar' 'srinivasa.prajwal@gmail.com'
+#
+#  I am making my contributions/submissions to this project solely in
+#  my personal capacity and am not conveying any rights to any
+#  intellectual property of any third parties.
+#																			 
+#	 This program is free software: you can redistribute it and/or modify
+#	 it under the terms of the GNU General Public License as published by
+#	 the Free Software Foundation, either version 3 of the License, or
+#	 (at your option) any later version.
+#																			 
+#	 This program is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	 GNU General Public License for more details.
+#																			 
+#	 You should have received a copy of the GNU General Public License
+#	 along with this program.  If not, see <http://www.gnu.org/licenses/
+
+mkdir -p $UPAN_SRC
+
+if [ ! -d "$UPAN_SRC/upanlibs" ]
+then
+  (cd $UPAN_SRC && git clone https://github.com/upandev/upanlibs.git)
+fi
+
+if [ ! -d "$UPAN_SRC/upanix" ]
+then
+  (cd $UPAN_SRC && git clone https://github.com/upandev/upanix.git)
+fi
+
+SUDO_PW=""
+if [ -f ~/.sudopw ]
+then
+  SUDO_PW=`cat ~/.sudopw`
+else
+  echo "Don't find .sudopw file. Create a (secure) file .sudopw containing sudo password"
+  exit 1
+fi
+
+(cd $UPAN_SRC/upanix && mkdir -p USBImage/mnt)
+
+IMAGE_DIR=$UPAN_SRC/upanix/USBImage/
+dd if=/dev/zero of=$IMAGE_DIR/300MUSB.img bs=512 count=614400
+dd if=/dev/zero of=$IMAGE_DIR/300MUSB_xhci.img bs=512 count=614400
+
+#create 2 partitions with gdisk - one 200 MB for uefi fat32 boot partition and another for upanix (will be formatted in mosfs)
+sgdisk -n 1:2048:200M -n 2:: $IMAGE_DIR/300MUSB.img
+sgdisk -n 1:2048:200M -n 2:: $IMAGE_DIR/300MUSB_xhci.img
+
+echo $IMAGE_DIR
+echo $SUDO_PW | sudo -S -i -- << EOF
+MOUNTP=`echo $SUDO_PW | sudo -S kpartx -av $IMAGE_DIR/300MUSB.img | head -1 | cut -d" " -f3`
+echo "Mount Device: \$MOUNTP"
+LOOP_DEV=\${MOUNTP::-2}
+echo "Loop Device: \$LOOP_DEV"
+
+mkdosfs -F32 /dev/mapper/\$MOUNTP
+
+mount /dev/mapper/\$MOUNTP $IMAGE_DIR/mnt/
+(cd $IMAGE_DIR/mnt && mkdir -p efi)
+cp -rf $UPANIX_TOOLS/grub_boot/. $IMAGE_DIR/mnt/efi/boot
+
+umount $IMAGE_DIR/mnt
+
+dmsetup remove /dev/mapper/\${LOOP_DEV}p1
+dmsetup remove /dev/mapper/\${LOOP_DEV}p2
+losetup -d /dev/\$LOOP_DEV
+EOF
